1ae35e683da9a45aef42d5979de6be9f9d5ff420
DmitryArhipenko <42931799+DmitryArhipenko@users.noreply.github.com>
2020-10-17 16:32:04 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.35369277eabbfb9f41d8e1c2d600fc7c.js"
+  "./precache-manifest.3ac273be2af8a58c62ae16871ec9bbbc.js"
 );
 
 self.addEventListener('message', (event) => {
